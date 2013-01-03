1ed1571ae40a2ce5273c130be54dcd5819c24d28
Daniel Novoa <daniel.novoa.77@gmail.com>
2013-01-03 12:43:21 +0100

    Kmom03 Lagt till IHasSQL

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -4,10 +4,10 @@
 *
 * @package LydiaCore
 */
-class CCGuestbook extends CObject implements IController {
+class CCGuestbook extends CObject implements IController, IHasSQL {
 
-  private $pageTitle = 'conFusion Guestbook Example';
-  private $pageHeader = '<h1>Guestbook Example</h1><p>Showing off how to implement a guestbook in conFusion. Now saving to database.</p>';
+  private $pageTitle = 'Lydia Guestbook Example';
+  private $pageHeader = '<h1>Guestbook Example</h1><p>Showing off how to implement a guestbook in Lydia. Now saving to database.</p>';
   private $pageMessages = '<h2>Current messages</h2>';
   
 
@@ -17,9 +17,28 @@ class CCGuestbook extends CObject implements IController {
   public function __construct() {
     parent::__construct();
   }
-  
 
-  /**
+
+/**
+* Implementing interface IHasSQL. Encapsulate all SQL used by this class.
+*
+* @param string $key the string that is the key of the wanted SQL-entry in the array.
+*/
+  public static function SQL($key=null) {
+   $queries = array(
+   'create table guestbook' => "CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));",
+   'insert into guestbook' => 'INSERT INTO Guestbook (entry) VALUES (?);',
+   'select * from guestbook' => 'SELECT * FROM Guestbook ORDER BY id DESC;',
+   'delete from guestbook' => 'DELETE FROM Guestbook;',
+   );
+   if(!isset($queries[$key])) {
+   throw new Exception("No such SQL query, key '$key' was not found.");
+}
+return $queries[$key];
+}
+
+
+/**
 * Implementing interface IController. All controllers must have an index action.
 */
   public function Index() { 
@@ -33,7 +52,7 @@ class CCGuestbook extends CObject implements IController {
 <p>
 <input type='submit' name='doAdd' value='Add message' />
 <input type='submit' name='doClear' value='Clear all messages' />
-<input type='submit' name='doCreate' value='Create db table' />
+<input type='submit' name='doCreate' value='Create database table' />
 </p>
 </form>
 ";
@@ -46,17 +65,6 @@ class CCGuestbook extends CObject implements IController {
     }
   }
   
-
-  /**
-   * Save a new entry to database.
-   */
-  private function CreateTableInDatabase() {
-    try {
-      $this->db->ExecuteQuery("CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));");
-    } catch(Exception$e) {
-      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
-    }
-  }
 
 /**
 * Handle posts from the form and take appropriate action.
@@ -75,37 +83,48 @@ class CCGuestbook extends CObject implements IController {
   }
   
 
-  /**
-   * Save a new entry to database.
-   */
+/**
+* Save a new entry to database.
+*/
+  private function CreateTableInDatabase() {
+    try {
+      $this->db->ExecuteQuery(self::SQL('create table guestbook'));
+    } catch(Exception$e) {
+      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
+    }
+  }
+  
+
+/**
+* Save a new entry to database.
+*/
   private function SaveNewToDatabase($entry) {
-    $this->db->ExecuteQuery('INSERT INTO Guestbook (entry) VALUES (?);', array($entry));
+    $this->db->ExecuteQuery(self::SQL('insert into guestbook'), array($entry));
     if($this->db->rowCount() != 1) {
       echo 'Failed to insert new guestbook item into database.';
     }
   }
   
 
-  /**
-   * Delete all entries from the database.
-   */
+/**
+* Delete all entries from the database.
+*/
   private function DeleteAllFromDatabase() {
-    $this->db->ExecuteQuery('DELETE FROM Guestbook;');
+    $this->db->ExecuteQuery(self::SQL('delete from guestbook'));
   }
   
   
 /**
-   * Read all entries from the database.
-   */
+* Read all entries from the database.
+*/
   private function ReadAllFromDatabase() {
     try {
       $this->db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      return $this->db->ExecuteSelectQueryAndFetchAll('SELECT * FROM Guestbook ORDER BY id DESC;');
+      return $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
     } catch(Exception $e) {
-      return array();   
+      return array();
     }
   }
+
   
-}
-
-
+} 
